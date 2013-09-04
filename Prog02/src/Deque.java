560742e0338b5589c9222dc9f325ef030eccc23b
Michal Velek <mvelek@gmail.com>
2013-09-04 20:51:12 +0200

    initial import

---
+++ Prog02/src/Deque.java
@@ -0,0 +1,45 @@
+import java.util.Iterator;
+
+public class Deque<Item> implements Iterable<Item> {
+
+	// construct an empty deque
+	public Deque() {
+
+	}
+
+	// is the deque empty?
+	public boolean isEmpty() {
+		return false;
+	}
+
+	// return the number of items on the deque
+	public int size() {
+		return 0;
+	}
+
+	// insert the item at the front
+	public void addFirst(Item item) {
+
+	}
+
+	// insert the item at the end
+	public void addLast(Item item) {
+
+	}
+
+	// delete and return the item at the front
+	public Item removeFirst() {
+		return null;
+	}
+
+	// delete and return the item at the end
+	public Item removeLast() {
+		return null;
+	}
+
+	// return an iterator over items in order from front to end
+	public Iterator<Item> iterator() {
+		return null;
+	}
+	
+}
