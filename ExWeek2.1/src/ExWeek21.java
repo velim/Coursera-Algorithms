64f88d8101f90d6b99436feec7afa65f61640f80
Michal Velek <mvelek@gmail.com>
2013-09-12 14:45:26 +0200

    formated

--- ExWeek2.1/src/ExWeek21.java
+++ ExWeek2.1/src/ExWeek21.java
@@ -1,13 +1,16 @@
 public class ExWeek21 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		int[] t = new In(args[0]).readAllInts();
+		
 		Integer[] a = new Integer[t.length];
+		
 		for (int i = 0; i < t.length; i++)
 			a[i] = t[i];
-		MyShell.sort(a, 4);
 		
+		MyShell.sort(a, 4);
+
 	}
 
 }
